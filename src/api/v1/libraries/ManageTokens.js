d57b3465a820ba2647678878d1f2b89944d813a5
AnushkaIshara <isharanushka@gmail.com>
2024-07-21 11:13:39 +0530

    Created ManageToken file

---
+++ src/api/v1/libraries/ManageTokens.js
@@ -0,0 +1,52 @@
+//--------Third party Libraries and Modules----------
+const jwt = require("jsonwebtoken");
+
+//---------------------Custom libraries and Modules ---------------
+const Config = require("../../../configurations");
+const { config } = require("dotenv");
+
+//-------------------Function to generate Tokens----------------------
+const GenerateTokens = (user) => {
+try {
+    //---------------create Playload--------------
+    const Playload = {id:user._id , userType: user.userType};
+
+    //----------------Generate Access Token--------------
+    const AccessToken = jwt.sign(Playload, Config.JWT_ACCESS_KEY ,{ expiresIn:"2min"});
+
+    return {
+        status:true,
+        AccessToken:AccessToken
+    }
+
+} catch (error) {
+    console.log(error);
+    return {
+        status:false,
+        AccessToken:null
+    }
+}
+}
+
+//----------------function to verify tokens-----
+const VerifyTokens = (token) => {
+    try {
+        //-----------------verify token-------------
+        const User = jwt.verify(token, Config.JWT_ACCESS_KEY);
+        return {
+            status:true,
+            tokenDetails:User
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            status:false,
+            tokenDetails:null
+        }
+    }
+}
+
+module.exports = {
+    GenerateTokens,
+    VerifyTokens,
+}
